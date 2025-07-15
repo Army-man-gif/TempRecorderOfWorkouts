3bdb7067be1014553ed126124c47d84e4c772ee5
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-15 22:06:55 +0100

    '.'

--- basePart/vite.config.js
+++ basePart/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fancy-toDoListForpersonaluse/docs",
+  base: "/TempRecorderOfWorkouts/docs",
   plugins: [react()],
   server: {
     open: true,
