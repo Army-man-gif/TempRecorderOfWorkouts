a932a3e7c0644a4de2019a05633f0343458a0560
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-15 17:56:07 +0100

    '.'

---
+++ basePart/src/main.jsx
@@ -0,0 +1,6 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+//sdfdss
+//createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render();
