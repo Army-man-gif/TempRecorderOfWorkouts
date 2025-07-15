5f104f6a30dbe430120a6239dfdf5f403f739912
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-15 19:25:45 +0100

    dsff

---
+++ basePart/src/databaseLogic.js
@@ -0,0 +1,60 @@
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  setDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbfNNxbNCQzp_ec_n4xvf5fD1zVb5S_zo",
+  authDomain: "temprecording.firebaseapp.com",
+  projectId: "temprecording",
+  storageBucket: "temprecording.firebasestorage.app",
+  messagingSenderId: "433465160677",
+  appId: "1:433465160677:web:075bbef2733a3af388f50c",
+  measurementId: "G-GBRJYH26QY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export async function updateData(id, newData, name) {
+  const docRef = doc(db, name, id);
+  await updateDoc(docRef, newData);
+}
+export async function cleanAll(name) {
+  try {
+    const fetch = await getDocs(collection(db, name));
+    await Promise.all(fetch.docs.map((doc) => deleteDoc(doc.ref)));
+  } catch (error) {
+    console.error("Error cleaning all documents:", error);
+    throw error;
+  }
+}
+export async function clean(name, id) {
+  await deleteDoc(doc(db, name, id));
+}
+export async function getData(name) {
+  const fetch = await getDocs(collection(db, name));
+  const data = [];
+  fetch.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
+  return data;
+}
+export async function addData(name, id, data) {
+  const docRef = doc(db, name, id);
+  await setDoc(docRef, data);
+}
