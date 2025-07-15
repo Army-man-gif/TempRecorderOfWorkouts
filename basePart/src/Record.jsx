10e5ad5c0c96b281d755ff6813eb14399e7e30cf
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-15 23:16:52 +0100

    '.'

--- basePart/src/Record.jsx
+++ basePart/src/Record.jsx
@@ -1,3 +1,4 @@
+import { useDrop } from "react-use";
 import {
   getData,
   addData,
@@ -5,9 +6,18 @@ import {
   updateData,
   cleanAll,
 } from "./databaseLogic.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function Record() {
-  const [user, setUser] = useState(() => prompt("Enter your name"));
+  const [user, setUser] = useState(() => prompt("Enter your username"));
+  useEffect(() => {
+    alert(
+      "This username is to be kept private. If you reveal this to others, they can access and alter your data. If you need to see ur username again for remembering type Yes in the following prompt",
+    );
+    const confirm = prompt("Yes or No?");
+    if (confirm == "Yes") {
+      alert(user);
+    }
+  }, [user]);
 }
 
 export default Record;
