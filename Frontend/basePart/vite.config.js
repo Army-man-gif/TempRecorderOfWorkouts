68539e013d0b1cfdcb82185fb3aea71c76b164db
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-25 19:11:55 +0100

    '.'

--- Frontend/basePart/vite.config.js
+++ Frontend/basePart/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/TempRecorderOfWorkouts/docs",
+  base: isProduction ? "/TempRecorderOfWorkouts/docs/" : "/",
   plugins: [react()],
 });
