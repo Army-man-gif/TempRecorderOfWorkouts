c32844d0a9371f1d1cab3d9cce228500b7c74e60
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-29 17:01:51 +0100

    '.'

--- Frontend/basePart/src/Record.jsx
+++ Frontend/basePart/src/Record.jsx
@@ -31,6 +31,7 @@ function Record() {
     async function load() {
       await User(user, passkey);
       await WorkoutListofToday();
+      console.log("Workout list of today:", WorkoutListofToday);
     }
     load(user);
   }, [user]);
