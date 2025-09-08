4b344c05a1cd011effc1ff2b380091a89881efe2
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-09-08 15:51:10 +0100

    '.'

--- Backend/workoutRecords/databaseForRecords/middleware/custom_crsf_tokenLogic.py
+++ Backend/workoutRecords/databaseForRecords/middleware/custom_crsf_tokenLogic.py
@@ -1,6 +1,9 @@
 from django.middleware.csrf import CsrfViewMiddleware
 
 class CustomCsrfMiddleware(CsrfViewMiddleware):
+    def __init__(self, get_response):
+        print("Custom CSRF middleware instantiated!")
+        super().__init__(get_response)
     def _get_token(self, request):
         print("Custom CSRF Middleware hit!")
         token = request.META.get('HTTP_X_CSRFTOKEN')
