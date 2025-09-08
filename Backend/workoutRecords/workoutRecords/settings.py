a874ec88390a4f622104ef6407edeb4d1ad716bf
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-09-08 13:03:10 +0100

    '.'

--- Backend/workoutRecords/workoutRecords/settings.py
+++ Backend/workoutRecords/workoutRecords/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     'django.middleware.common.CommonMiddleware',
     #"django.middleware.csrf.CsrfViewMiddleware",
-    'workoutRecords.middleware.custom_crsf_tokenLogic.CustomCsrfMiddleware'
+    'databaseForRecords.middleware.custom_crsf_tokenLogic.CustomCsrfMiddleware'
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
