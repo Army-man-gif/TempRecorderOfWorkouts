164490e57dd1bf60d502643c75eba46e7739861c
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-09-08 15:57:08 +0100

    '.'

--- Backend/workoutRecords/databaseForRecords/middleware/custom_crsf_tokenLogic.py
+++ Backend/workoutRecords/databaseForRecords/middleware/custom_crsf_tokenLogic.py
@@ -10,3 +10,9 @@ class CustomCsrfMiddleware(CsrfViewMiddleware):
         if token:
             return token
         return super()._get_token(request)
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        token_from_header = request.META.get('HTTP_X_CSRFTOKEN')
+        if token_from_header:
+            request.META['CSRF_COOKIE'] = token_from_header
+            print(f"Custom CSRF Middleware hit! Token from header: {token_from_header}")
+        return super().process_view(request, callback, callback_args, callback_kwargs)
--- Backend/workoutRecords/workoutRecords/settings.py
+++ Backend/workoutRecords/workoutRecords/settings.py
@@ -43,17 +43,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    #"django.middleware.csrf.CsrfViewMiddleware",
-    'databaseForRecords.middleware.custom_crsf_tokenLogic.CustomCsrfMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "databaseForRecords.middleware.custom_crsf_tokenLogic.CustomCsrfMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
+#"django.middleware.csrf.CsrfViewMiddleware",
 CSRF_COOKIE_SECURE = True      
 CSRF_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_HTTPONLY = False
