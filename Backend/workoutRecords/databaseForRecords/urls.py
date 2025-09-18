72a6a4c8d15e2746fc2891e83502e4b0e039155d
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-09-18 20:43:55 +0100

    '.'

--- Backend/workoutRecords/databaseForRecords/urls.py
+++ Backend/workoutRecords/databaseForRecords/urls.py
@@ -6,22 +6,12 @@ urlpatterns = [
     path("getToken/",views.get_csrf_token,name="getToken"),
     
     path("GetorMakeUser/", views.GetorMakeUser,name="GetorMakeUser"),
-    path("validateUser/", views.validateUser,name="validateUser"),
-    path("deleteUser/", views.deleteUser,name="deleteUser"),
     
     path("login/", views.loginView,name="login"),
     path("logout/", views.logoutView,name="logout"),
     
-    path("updateExercise/", views.updateExercise,name="updateExercise"),
     path("batchupdateExercise/", views.batchupdateExercise,name="batchupdateExercise"),
-    path("deleteExercise/", views.deleteExercise,name="deleteExercise"),
-    
-    
-    path("addWorkout/", views.addWorkout,name="addWorkout"),
-    path("updateWorkout/", views.updateWorkout,name="updateWorkout"),
-    path("deleteWorkout/",views.deleteWorkout,name="deleteWorkout"),
-    
-    path("getAllExercisesbasedOnDate/", views.getAllExercisesbasedOnDate,name="getAllExercisesbasedOnDate"),
+
     path("getAll/", views.getAll,name="getAll"),
 
 ]
